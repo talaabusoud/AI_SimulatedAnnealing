0d82e8ffdf1640c6a31d00d39aa676013eff096a
Tala Abu Soud <talaashrafsoud@gmail.com>
2024-12-21 03:18:35 +0200

    Create CoolingFunction.java

---
+++ src/main/java/edu/najah/ai_vrp/CoolingFunction.java
@@ -0,0 +1,9 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.najah.ai_vrp;
+
+public abstract class CoolingFunction {
+    public abstract int apply(int currentTemp,int iteration);
+}
