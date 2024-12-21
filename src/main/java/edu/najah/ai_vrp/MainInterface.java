0234e64aba58ba26c14b01772e28c77ea0dac351
Tala Abu Soud <talaashrafsoud@gmail.com>
2024-12-21 03:20:21 +0200

    Update MainInterface.java

--- src/main/java/edu/najah/ai_vrp/MainInterface.java
+++ src/main/java/edu/najah/ai_vrp/MainInterface.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package edu.najah.ai_tsp;
+package edu.najah.ai_vrp;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
